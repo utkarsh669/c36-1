6280551cd1cd15613890c2d1b153cd156cf7f91d
utkarsh669 <73828827+utkarsh669@users.noreply.github.com>
2021-03-19 19:56:26 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,60 @@
+var ball,ballposition,position,database;
+
+function setup(){
+    createCanvas(500,500);
+    database=firebase.database()
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    ballposition=database.ref("ball/position")
+ballposition.on("value",readposition,showerror)
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function writePosition(x,y){
+    database.ref("ball/position").set({
+     x:ball.x + x,
+    y:ball.y + y,
+    })
+
+}
+function readposition (data) {
+position=data.val()
+ball.x=position.x;
+ball.y=position.y;
+
+}
+function showerror (){
+console.log("error")
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
